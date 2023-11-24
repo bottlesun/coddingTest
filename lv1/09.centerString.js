764d63d9d8f34d40fbc57bfdc8e73f3e49cff698
bottlesun <bottlesun3514@gmail.com>
2023-11-24 17:56:20 +0900

    Update

---
+++ lv1/09.centerString.js
@@ -0,0 +1,26 @@
+/** 가운데 글자 가지고 오기
+
+* 문제 설명
+* 단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
+
+* 제한사항
+* s는 길이가 1 이상, 100이하인 스트링입니다.
+
+
+* 입출력 예
+* s	return
+* "abcde"	"c"
+* "qwer"	"we"
+*
+*/
+
+const solution = (s) => {
+  let arr = s.split("")
+  if(arr.length %2==0){
+      return arr[arr.length / 2 - 1] + arr[arr.length / 2]
+  } else {
+      return arr[Math.floor(arr.length / 2) ]
+  }
+}
+
+solution("abcde");
